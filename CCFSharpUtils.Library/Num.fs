981f26ae91322105d1e61ef4c8a44c722bf39dd9
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-01-14 17:21:04 +0900

    Add neg, pos, and isNotZero funcs to Num

--- CCFSharpUtils.Library/Num.fs
+++ CCFSharpUtils.Library/Num.fs
@@ -1,13 +1,19 @@
 namespace CCFSharpUtils.Library
 
-open System
-open System.Globalization
-
 [<RequireQualifiedAccess>]
 module Num =
 
     let inline isZero (n: ^a) =
         n = LanguagePrimitives.GenericZero<'a>
 
+    let inline isNonZero (n: ^a) =
+        not <| isZero n
+
     let inline isOne (n: ^a) =
         n = LanguagePrimitives.GenericOne<'a>
+
+    let inline isNeg (n: ^a) =
+        n < LanguagePrimitives.GenericZero<'a>
+
+    let inline isPos (n: ^a) =
+        n > LanguagePrimitives.GenericZero<'a>
