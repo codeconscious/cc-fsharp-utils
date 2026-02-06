c7cd8b09e07d8bba616c6236b83891ec150f9b14
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-06 22:36:35 +0900

    Rework Num module func

--- CCFSharpUtils.Library/Num.fs
+++ CCFSharpUtils.Library/Num.fs
@@ -12,11 +12,14 @@ module Num =
     let inline isOne (n: ^a) =
         n = LanguagePrimitives.GenericOne<'a>
 
+    /// If the value is less than 0, returns true; otherwise, false.
     let inline isNeg (n: ^a) =
         n < LanguagePrimitives.GenericZero<'a>
 
-    let inline isZeroOrNeg (n: ^a) =
-        n < LanguagePrimitives.GenericOne<'a>
-
+    /// If the value is greater than 0, returns true; otherwise, false.
     let inline isPos (n: ^a) =
         n > LanguagePrimitives.GenericZero<'a>
+
+    /// If the value is less than or equal to 0, returns true; otherwise, false.
+    let inline isZeroOrNeg (n: ^a) =
+        isZero n || isNeg n
