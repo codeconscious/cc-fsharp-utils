0984bfc6166e1e9b22ada190e78ebe58721a4231
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-06 16:00:58 +0900

    Add explicit types

--- CCFSharpUtils.Library/Collections/List.fs
+++ CCFSharpUtils.Library/Collections/List.fs
@@ -6,22 +6,22 @@ open System
 [<RequireQualifiedAccess>]
 module List =
 
-    let isNotEmpty lst =
+    let isNotEmpty (lst: 'a list) : bool =
         not <| List.isEmpty lst
 
-    let anyNotEmpty lsts =
+    let anyNotEmpty (lsts: 'a list list) : bool =
         lsts |> List.exists isNotEmpty
 
-    let allNotEmpty lsts =
+    let allNotEmpty (lsts: 'a list list) : bool =
         lsts |> List.forall isNotEmpty
 
-    let doesNotContain x =
+    let doesNotContain (x: 'a) : 'a list -> bool =
         not << List.contains x
 
-    let headElse alt =
+    let headElse (alt: 'a) : 'a list -> 'a =
         List.tryHead >> Option.defaultValue alt
 
-    let takeLast count lst =
+    let takeLast (count: int) (lst: 'a list) : 'a list =
         if count <= 0 then
             List.empty
         else
@@ -30,19 +30,19 @@ module List =
             |> List.truncate count
             |> List.rev
 
-    let hasOne lst =
+    let hasOne (lst: 'a list) : bool =
         lst |> List.length |> Num.isOne
 
-    let hasMultiple lst =
+    let hasMultiple (lst: 'a list) : bool =
         lst |> List.length |> (<) 1
 
-    let ensureOne emptyErr multipleErr lst =
+    let ensureOne (emptyErr: 'a) (multipleErr: 'a) (lst: 'b list) : Result<'b list, 'a> =
         match lst with
         | []  -> Error emptyErr
         | [x] -> Ok [x]
         | _   -> Error multipleErr
 
-    let ensureSize targetSize tooSmallErr tooLargeErr (lst: 'b list): Result<'b list,'a> =
+    let ensureSize (targetSize: int) (tooSmallErr: 'err) (tooLargeErr: 'err) (lst: 'a list): Result<'a list, 'err> =
         if Num.isNeg targetSize then
             invalidArg (nameof targetSize) "Target size cannot be negative."
 
@@ -51,39 +51,43 @@ module List =
         | LT -> Error tooSmallErr
         | GT -> Error tooLargeErr
 
-    let tryGetSingle emptyErr multipleErr lst =
+    let tryGetSingle (emptyErr: 'err) (multipleErr: 'err) (lst: 'a list) : Result<'a, 'err> =
         match lst with
         | []  -> Error emptyErr
         | [x] -> Ok x
         | _   -> Error multipleErr
 
-    let containsIgnoreCase txt (lst: string list) : bool =
+    let containsIgnoreCase (txt: string) (lst: string list) : bool =
         lst |> List.exists (fun x -> String.Equals(x, txt, StringComparison.OrdinalIgnoreCase))
 
-    let anyContainsIgnoreCase txt =
+    let anyContainsIgnoreCase (txt: string) : string list list -> bool =
         List.exists (containsIgnoreCase txt)
 
     /// If the list is empty, returns None. Otherwise, wraps the list in Some.
-    let toOption lst = if List.isEmpty lst then None else Some lst
+    let toOption (lst: 'a list) : 'a list option =
+        if List.isEmpty lst then None else Some lst
 
     /// If the list is empty, returns the specified Error. Otherwise, wraps the list in Ok.
-    let toResult err lst =
+    let toResult (err: 'err) (lst: 'a list) : Result<'a list, 'err> =
         if List.isEmpty lst then Error err else Ok lst
 
     /// If the list is empty, returns the specified Error.
     /// Otherwise, converts it to a NonEmptyList wrapped in Ok.
-    let toNonEmptyListResult err = function
+    let toNonEmptyListResult (err: 'err) : 'a list -> Result<'a NonEmptyList, 'err> =
+        function
         | [] -> Error err
         | lst -> Ok (NonEmptyList.ofList lst)
 
     /// If the list is empty, returns the specified Error.
     /// Otherwise, converts it to a NonEmptySeq wrapped in Ok.
-    let toNonEmptySeqResult err = function
+    let toNonEmptySeqResult (err: 'err) : 'a list -> Result<'a NonEmptySeq, 'err> =
+        function
         | [] -> Error err
         | lst -> Ok (NonEmptySeq.ofList lst)
 
     /// If the list is empty, returns the specified Error.
     /// Otherwise, converts it to a NonEmptySet wrapped in Ok.
-    let toNonEmptySetResult err = function
+    let toNonEmptySetResult (err: 'err) : 'a list -> Result<'a NonEmptySet, 'err> =
+        function
         | [] -> Error err
         | lst -> Ok (NonEmptySet.ofList lst)
