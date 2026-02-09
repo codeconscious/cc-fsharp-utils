7a6a75e0b2447f6a5ddc3ba42c63776af38698f1
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-02-09 18:35:14 +0900

    Remove validation funcs

--- CCFSharpUtils.Library/IO/Dir.fs
+++ CCFSharpUtils.Library/IO/Dir.fs
@@ -13,19 +13,3 @@ module Dir =
         if Directory.Exists directoryName
         then Ok (DirectoryInfo directoryName)
         else Error err
-
-    /// If the directory exists, returns it wrapped in Success.
-    /// Otherwise, returns the error wrapped in Failure.
-    /// Intended to be used in applicative validation chains.
-    let validateExists (err: 'err) (directoryName: string) : Validation<'err, string> =
-        if Directory.Exists directoryName
-        then Success directoryName
-        else Failure err
-
-    /// If the directory exists, returns its DirectoryInfo wrapped in Success.
-    /// Otherwise, returns the error wrapped in Failure.
-    /// Intended to be used in applicative validation chains.
-    let validateToDirInfo (err: 'err) (directoryName: string) : Validation<'err, DirectoryInfo> =
-        if Directory.Exists directoryName
-        then Success (DirectoryInfo directoryName)
-        else Failure err
--- CCFSharpUtils.Library/IO/File.fs
+++ CCFSharpUtils.Library/IO/File.fs
@@ -13,19 +13,3 @@ module File =
         if File.Exists fileName
         then Ok (FileInfo fileName)
         else Error err
-
-    /// If the file exists, returns it wrapped in Success.
-    /// Otherwise, returns the error wrapped in Failure.
-    /// Intended to be used in applicative validation chains.
-    let validateExists (err: 'err) (fileName: string) : Validation<'err, string> =
-        if File.Exists fileName
-        then Success fileName
-        else Failure err
-
-    /// If the file exists, returns its FileInfo wrapped in Success.
-    /// Otherwise, returns the error wrapped in Failure.
-    /// Intended to be used in applicative validation chains.
-    let validateToFileInfo (err: 'err) (fileName: string) : Validation<'err, FileInfo> =
-        if File.Exists fileName
-        then Success (FileInfo fileName)
-        else Failure err
