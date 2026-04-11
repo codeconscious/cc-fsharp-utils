142e8d41a917bc2e32e7b91c5b0e51ba3faee60a
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-04-11 15:04:12 +0900

    Add type alias and increment version

---
+++ CCFSharpUtils/Types.fs
@@ -0,0 +1,21 @@
+namespace CCFSharpUtils
+
+open FSharpPlus.Data
+
+[<AutoOpen>]
+module Types =
+
+    // A non-empty list that guarantees at least one element.
+    // Alias of FSharpPlus.Data.NonEmptyList.
+    type nlist<'a> = NonEmptyList<'a>
+
+    // A non-empty sequence that guarantees at least one element.
+    // Alias of FSharpPlus.Data.NonEmptySeq.
+    type nseq<'a> = NonEmptySeq<'a>
+
+    // A non-empty set that guarantees at least one element.
+    // Alias of FSharpPlus.Data.NonEmptySet.
+    type nset<'a when 'a: comparison> = NonEmptySet<'a>
+
+    // Alias of System.Text.StringBuilder.
+    type SB = System.Text.StringBuilder
