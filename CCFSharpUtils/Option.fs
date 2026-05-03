8c2a0b7284e0b5a5afb4ce95e13d71ad3decfc76
CodeConscious <50596087+codeconscious@users.noreply.github.com>
2026-05-03 11:23:51 +0900

    Add Option.isPos and isNeg

--- CCFSharpUtils/Option.fs
+++ CCFSharpUtils/Option.fs
@@ -8,3 +8,11 @@ module Option =
         opt
         |> Option.map mapping
         |> Option.defaultValue alt
+
+    /// Gives a number wrapped in Some if it is less than zero. Otherwise, None.
+    let inline isNeg (n: ^a) : ^a option when ^a : comparison and ^a : (static member get_Zero : unit -> ^a) =
+        if Num.isNeg n then Some n else None
+
+    /// Gives a number wrapped in Some if it is greater than zero. Otherwise, None.
+    let inline isPos (n: ^a) : ^a option when ^a : comparison and ^a : (static member get_Zero : unit -> ^a) =
+        if Num.isPos n then Some n else None
